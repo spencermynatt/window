cbdbd9c8305ef48e85ce1142b9d2655ac9f8b6b8
spencermynatt <75866352+spencermynatt@users.noreply.github.com>
2021-01-19 14:19:08 -0600

    Update creationof_window.cpp

--- creationof_window.cpp
+++ creationof_window.cpp
@@ -55,12 +55,12 @@ int main()
    unsigned int VBO;
   
     glGenBuffers(1, &VBO);
- 
+      glGenVertexArrays(1, &VBO); //THESE TWO YOU FORGOT
+    glBindVertexArray(VBO); //
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-      glGenVertexArrays(1, &VBO); //THESE TWO YOU FORGOT
-    glBindVertexArray(VBO); //
+
 	 glEnableVertexAttribArray(0);
 
    
